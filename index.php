f20bbe1997b0241d3cb4552b28590decf7c25d46
mgeekoficial <mysticalgeekoficial@gmail.com>
2019-11-29 00:54:18 -0300

    uso da classe page

--- index.php
+++ index.php
@@ -2,15 +2,20 @@
 
 require_once("vendor/autoload.php");
 
+//use \Slim\Slim;
+use \mgeekoficial\Page;
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
+
+	$page = new Page();
+	$page->setTpl("index");
     
-	$sql = new mgeekoficial\DB\Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
+	//$sql = new mgeekoficial\DB\Sql();
+	//$results = $sql->select("SELECT * FROM tb_users");
+	//echo json_encode($results);
 
 });
 
