be5c9cf0bc02a0274205d612b106bc3a7f4219aa
mgeekoficial <mysticalgeekoficial@gmail.com>
2019-11-29 12:55:09 -0300

    page da administração

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use mgeekoficial\Page;
+use mgeekoficial\PageAdmin;
 $app = new Slim();
 
 $app->config('debug', true);
@@ -11,6 +12,18 @@ $app->config('debug', true);
 $app->get('/', function() {
 
 	$page = new Page();
+	$page->setTpl("index");
+    
+	//$sql = new mgeekoficial\DB\Sql();
+	//$results = $sql->select("SELECT * FROM tb_users");
+	//echo json_encode($results);
+
+});
+
+
+$app->get('/admin', function() {
+
+	$page = new PageAdmin();
 	$page->setTpl("index");
     
 	//$sql = new mgeekoficial\DB\Sql();
